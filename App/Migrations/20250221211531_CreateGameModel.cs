1631bf45aca41549d8ffb4485da8d1f38b2c4ac6
troopmarshall <mohamedyaser.315@gmail.com>
2025-02-21 23:20:21 +0200

    test DB

---
+++ App/Migrations/20250221211531_CreateGameModel.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace App.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateGameModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GameName",
+                table: "Scores");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TimeStamp",
+                table: "Scores",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<int>(
+                name: "GameID",
+                table: "Scores",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserName",
+                table: "Users",
+                column: "UserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Scores_GameID",
+                table: "Scores",
+                column: "GameID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Scores_Games_GameID",
+                table: "Scores",
+                column: "GameID",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Scores_Games_GameID",
+                table: "Scores");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_UserName",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Scores_GameID",
+                table: "Scores");
+
+            migrationBuilder.DropColumn(
+                name: "GameID",
+                table: "Scores");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TimeStamp",
+                table: "Scores",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GameName",
+                table: "Scores",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
