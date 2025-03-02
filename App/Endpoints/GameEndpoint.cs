a14c2aac3c1ec6cf484c02b379694083468def7b
troopmarshall <mohamedyaser.315@gmail.com>
2025-03-02 02:01:34 +0200

    GET for Game

--- App/Endpoints/GameEndpoint.cs
+++ App/Endpoints/GameEndpoint.cs
@@ -2,11 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using App.Database;
+using Microsoft.EntityFrameworkCore;
 namespace App.Endpoints
 {
-    public class GameEndpoint
+    public static class GameEndpoint
     {
-        
+        public static RouteGroupBuilder MapGameEndpoints(this WebApplication app)
+        {
+            var group = app.MapGroup("games");
+
+            group.MapGet("/", async (ApplicationDbContext context) =>
+                    await context.Games.AsNoTracking().ToListAsync());
+
+            group.MapGet("/{id}", async (int id, ApplicationDbContext context) =>
+            {
+                var game = await context.Games.Where(g => g.Id == id).Include(x => x.Scores).FirstOrDefaultAsync();
+                return game is null ? Results.NotFound("There's No such Game") :
+                Results.Ok(game);
+            }
+            ).WithName("GetGame");
+
+
+            return group;
+        }
     }
 }
