788d83650e91f3f16612f464033bf3b9d81b3e78
troopmarshall <mohamedyaser.315@gmail.com>
2025-03-07 00:36:00 +0200

    validation for user signup

--- App/Validators/UserValidator.cs
+++ App/Validators/UserValidator.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using App.Database;
 using App.Dtos;
 using FluentValidation;
@@ -18,23 +13,30 @@ namespace App.Validators
         {
             _context = context;
 
-            RuleFor(u => u.Name).NotEmpty();
+            RuleFor(u => u.Name).NotEmpty().WithMessage("can't be empty.");
+
             RuleFor(u => u.Email)
-            .EmailAddress()
-            .NotEmpty()
+            .NotEmpty().WithMessage("can't be empty.")
+            .EmailAddress().WithMessage("Must be a valid email address")
             .MustAsync(async (email, cancellationToken) =>
             {
                 return !await _context.Users.AnyAsync(u => u.Email == email, cancellationToken);
             }).WithMessage("Email already exists.");
 
             RuleFor(u => u.UserName)
-            .MinimumLength(4)
-            .NotEmpty()
+            .MinimumLength(4).WithMessage("Must be at least 4 charachters")
+            .NotEmpty().WithMessage("this field can't be empty")
             .MustAsync(async (username, cancellationToken) =>
             {
-                return ! await _context.Users.AnyAsync(u => u.UserName == username, cancellationToken);
-            })
-            .WithMessage("UserName Already Exists");
+                return !await _context.Users.AnyAsync(u => u.UserName == username, cancellationToken);
+            }).WithMessage("UserName Already Exists");
+
+            RuleFor(u => u.Password)
+            .NotEmpty().WithMessage("this field can't be empty")
+            .MinimumLength(6).WithMessage("Must be at least 4 charachters")
+            .Matches(@"[A-Z]+").WithMessage("Must Contain at least 1 uppercase letter")
+            .Matches(@"[A-Z]+").WithMessage("Must Contain at least 1 lowercase letter")
+            .Matches(@"[0-9]+").WithMessage("Must Contain at least 1 number");
         }
 
     }
